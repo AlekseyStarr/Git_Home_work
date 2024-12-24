08af10113a19e4004d2d88e23b60512974e39f4d
aas <a.startsev1976@gmail.com>
2024-12-24 11:22:03 +0300

    homework_10_2 done

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,7 +5,9 @@ from src.masks import get_mask_account, get_mask_card_number
 
 # Тест правильной маскировки номера карты
 @pytest.mark.parametrize("card_number, expected",
-                         [("1596837868705199", "1596 83** **** 5199")])
+                         [("1596837868705199", "1596 83** **** 5199"),
+                          ("159683786870519", "Ошибка, проверьте правильность ввода"),
+                          ("", "Ошибка, проверьте правильность ввода")])
 
 
 def test_get_mask_card_number(card_number, expected):
@@ -14,7 +16,9 @@ def test_get_mask_card_number(card_number, expected):
 
 # Тест правильной маскировки номера счета
 @pytest.mark.parametrize("acc_number, expected",
-                         [("73654108430135874305", "**4305")])
+                         [("73654108430135874305", "**4305"),
+                          ("7365410843013587430", "Ошибка, проверьте правильность ввода"),
+                          ("", "Ошибка, проверьте правильность ввода")])
 
 
 def test_get_mask_account(acc_number, expected):
