604a91e141f93ef8b75c64e9c7e326252687fa15
aas <a.startsev1976@gmail.com>
2025-01-12 16:03:51 +0300

    homework_10_3 done

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from typing import Any
+
+
+def log(filename: Any = None) -> Any:
+    """Декоратор логирует вызов функций-обработчиков и результаты их работы"""
+
+    def my_decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename is not None:
+                    with open(filename, "w") as file:
+                        file.write("my function OK")
+                return result
+            except Exception as e:
+                if filename is not None:
+                    with open(filename, "w") as file:
+                        file.write(f"my function error:{e}. Input: {args, kwargs}")
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return my_decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    """Функция для проверки работы декоратора"""
+    return x / y
+
+
+my_function(10, 2)
