82646f689b3fc9e10de3c35b6acc7f414e0faf58
aas <a.startsev1976@gmail.com>
2025-01-29 13:34:56 +0300

    homework_10_3 done

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,3 @@
-import csv
 import json
 import logging
 
@@ -18,21 +17,6 @@ def read_file_json(filename: str = None) -> list:
         with open(filename, encoding="utf-8") as file:  # Открытие и считывание файла формата JSON
             reading = json.load(file)  # Преобразование JSON файла в пайтон обьект
             logger.info("Оконили выгрузку с файла JSON формата")
-        return reading
-    except Exception as e:
-        logger.error(f"Произошла ошибка: {e}")
-        return []  # В случае ошибки возвращает пустой список
-
-
-def read_file_csv(filename: str = None) -> list:
-    """Функция принимающая путь к файлу, считывает информацию c CSV файла"""
-    try:
-        """Это логер для функции read_file_csv"""
-        logger.info("Начал выгрузку с файла csv формата")
-        with open(filename, encoding="utf-8") as file:  # Открытие и считывание файла формата CSV
-            reading_csv = csv.DictReader(file, delimiter=";")
-            reading = [read for read in reading_csv]  # Считывание файла методом цикла
-        logger.info("Окончили выгрузку с файла csv формата")
         return reading
     except Exception as e:
         logger.error(f"Произошла ошибка: {e}")
