e7f1dd524b959c0dcc1aabb56f19bc10f4437afa
aas <a.startsev1976@gmail.com>
2025-01-29 13:58:48 +0300

    homework_10_3 done

--- src/masks.py
+++ src/masks.py
@@ -1,32 +1,29 @@
+from typing import Union
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("../masks.log")
-file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler = logging.FileHandler('../masks.log')
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
 
 
-def get_mask_card_number(number: str) -> str:
-    """Маскирует номер карты"""
+def get_mask_card_number(card_number: Union[str]) -> str | None:
+    """Возвращает замаскированный номер карты в виде строки"""
     logger.info("Начали маскировку карты")
-    if len(number) < 16 or isinstance(number, int):
-        return "Ошибка, проверьте правильность ввода."
+    if card_number.isdigit() and len(card_number) == 16:
+        return f"{card_number[0:-12]} {card_number[-12:-10]}{"*" * 2} {"*" * 4} {card_number[-4:]}"
     else:
-        mask_number = number[0:-12] + " " + number[-12:-10] + 2 * "*" + " " + 4 * "*" + " " + number[-4:]
-    logger.info("Окончили маскировку карты")
-    return mask_number
+        logger.info("Окончили маскировку карты")
+        return "Ошибка, проверьте правильность ввода"
 
 
-def get_mask_account(account: str) -> str:
-    """Маскирует номер счета"""
-    """Это логер для функции get_mask_account"""
+def get_mask_account(acc_number: Union[str]) -> str | None:
+    """Возвращает замаскированный номер учетной записи в виде строки"""
     logger.info("Начали маскировку номера счета")
-    inter = str(account)
-    if len(account) < 20:
-        return "Ошибка, проверьте правильность ввода."
+    if acc_number.isdigit() and len(acc_number) == 20:
+        return f"{'*' * 2}{acc_number[-4::]}"
     else:
-        account = "Счет " + 2 * "*" + inter[-4:]
         logger.info("Окончили маскировку номера счета")
-    return account
+        return "Ошибка, проверьте правильность ввода"
