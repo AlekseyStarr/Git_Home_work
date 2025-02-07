408006fa47983dd002afc03faf9bd9b76357b486
aas <a.startsev1976@gmail.com>
2025-02-07 14:37:16 +0300

    homework_10_3 done

--- mains.py
+++ mains.py
@@ -1,5 +1,5 @@
 from src.widget import get_data, mask_account_card
-from src.utils import read_file_json
+from src.utils import read_file_json, read_file_excel
 
 user_input = input("Введите данные счета или карты: ")
 masked_output = (mask_account_card(user_input))
@@ -8,3 +8,4 @@ masked_output = (mask_account_card(user_input))
 print(masked_output)
 print(get_data("2024-03-11T02:26:18.671407"))
 print(read_file_json('data/operations.json'))
+print(read_file_excel('data/transactions_excel.xlsx'))
