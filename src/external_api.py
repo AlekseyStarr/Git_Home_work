2f784a22b5e476bdea418450720d293f06c3584d
aas <a.startsev1976@gmail.com>
2025-02-09 13:16:37 +0300

    homework_10_3 done

--- src/external_api.py
+++ src/external_api.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 
 def check_currency(transaction: dict) -> float:
@@ -11,12 +12,8 @@ def check_currency(transaction: dict) -> float:
     amount = float(transaction["operationAmount"]["amount"])  # получение суммы траты
     currency = transaction["operationAmount"]["currency"]["code"]  # получение валюты
     if currency != "RUB":
-        API_KEY = os.getenv("API_TOKEN")
         url = f"https://api.apilayer.com/exchangerates_data/latest?symbols=RUB&base={currency}"
-
         headers = {"apikey": f"{API_KEY}"}
-
         response = requests.get(url, headers=headers)
-
         return round(response.json()["rates"]["RUB"] * amount, 2)
     return amount
